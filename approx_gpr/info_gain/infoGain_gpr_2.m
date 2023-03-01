3ccd066d3b4c447c653895b8a0babf1cf0cf000d
Phil Parisi <philparisi@uri.edu>
2023-03-01 12:19:34 -0500

    work on info gain

--- approx_gpr/info_gain/infoGain_gpr_2.m
+++ approx_gpr/info_gain/infoGain_gpr_2.m
@@ -1,29 +1,33 @@
-% Comparing Fast GPR (SlimData) vs. Full GPR  (RawData)
+% Fast Forward Selection to Speed up Sparse GPR
+% Authors: Seeger, Williams, Lawrence
 % Programmer: Parisi
-% Date: 31May2022
+% Date: 18May2022
 
 % GOAL
-    % This script aims to generate figures of relative comparison between
-    % different GPR methods (full vs fast)
+    % Input: raw training data of n total points (inputs and outputs)
+    % Output: down-selected subset of d points from n total pts
+        % these will then be used as the inputs for the GPR model
+
+% This script should perform the repeated selection of slim data points
+% information gain will be calculated, new point added to slim data, repeat
+% model updates are needed, though this is not fully understood yet
+% are updates done to hyperparameters? the likelihood assumptions? idk tbh
+
 
 %%
-addpath("gpr_functions/")
+addpath("../../gpr_functions/")
 
 %%
 %%%%%%%%% Bring in Raw Data
 clc, clear all, format compact, %close all
 
-%seamounts
-load("~/Data/approx_methods_datasets/matlab_gpr_datasets/GPR_dataset_Seamounts_20001pts_2D.mat")
-
-%load("~/Data/approx_methods_datasets/matlab_gpr_datasets/A_random_training_data_gpr_40.mat")
+load("~/Data/approx_methods_datasets/matlab_gpr_datasets/A_random_training_data_gpr_40.mat")
 %load("~/Data/approx_methods_datasets/matlab_gpr_datasets/B_random_training_data_gpr_5000.mat")
 nnum = length(X);
 
 disp(strcat("...training data with ", num2str(nnum), " datapoints loaded..."))
 clearvars -except nnum X Y
 
-% X and Y must be VERTICAL!
 
 %% SELECT MULTIPLE POINTS (GROUP OF INCLUSION POINTS)
 
@@ -31,11 +35,14 @@ clc, clearvars -except nnum X Y, close all
 %Downsampling as Described in Paper
 
 % total points wanted in slim data [threshold for ending algo]
-totalpts_slimdata = round(0.25*nnum);
+totalpts_slimdata = round(0.5*nnum);
 
 %%%%%%%%%  Hyperparamters
-sigma = 0.8;
-sigmasq = sigma^2;
+hp.L = 10;
+hp.sigma_p = 1.2;
+hp.sigma_n = 0.5;
+hp.kerneltype = 'sparse';       %exact or sparse
+
 % lengthscale and process noise currently stored in kernel function
 
 %%%%%%%%%  Indexing Variables
@@ -55,15 +62,15 @@ R(R==inclusion_ind) = [];
 %%%%%%%%%  CURRENT TERMS
 
 % beginning terms
-K_I_I = K_Function(X(I),X(I));      % Self Covariance, I vs. I, dxd
-K_I_dot = K_Function(X(I),X(:));    % Joint Covariance, I vs. All, dxn
+K_I_I = K_Function(X(I),X(I),hp);      % Self Covariance, I vs. I, dxd
+K_I_dot = K_Function(X(I),X(:),hp);    % Joint Covariance, I vs. All, dxn
 P_I = inv(K_I_I) * K_I_dot;         % !?!?INVERSE!?!? dxd * dxn
 
 % cholesky decomp of K_I_I
 L = chol(K_I_I,'lower');        % dxd    % Lower Triangular
 V = inv(L)*K_I_dot;             % dxn
 %Ident_I = eye(length(I));       % dxd    % Identity Matrix
-M = sigmasq*eye(length(I)) + V*V';     % dxd    % Noisy Covariance maybe? 
+M = hp.sigma_p^2*eye(length(I)) + V*V';     % dxd    % Noisy Covariance maybe? 
 
 % cholesky decomp of M
 L_M = chol(M,'lower');          % dxd
@@ -97,15 +104,15 @@ while length(I) < totalpts_slimdata         % max pts in Slim Data
         q_i = q(R_ind);
 
         % This is becoming imaginary when p_i > sigmasq
-        l_i = sqrt(round(K_Function(X(R_ind),X(R_ind)) - p_i, 5)); 
+        l_i = sqrt(round(K_Function(X(R_ind),X(R_ind),hp) - p_i, 5)); 
 
-        epsilon_i = 1/((sigma/l_i)^2 + 1 - q_i);
-        kappa_i = epsilon_i*(1+2*(sigma/l_i)^2);
+        epsilon_i = 1/((hp.sigma_p/l_i)^2 + 1 - q_i);
+        kappa_i = epsilon_i*(1+2*(hp.sigma_p/l_i)^2);
 
         % info gain equation
-        info_gain(R_ind,1) = -log(sigma/l_i) + ... % NATURAL LOG?
+        info_gain(R_ind,1) = -log(hp.sigma_p/l_i) + ... % NATURAL LOG?
             -0.5*(log(epsilon_i) + ...
-            epsilon_i*(1-kappa_i)*(sigma^-2)*((Y(R_ind)-mu(R_ind))^2) + ...
+            epsilon_i*(1-kappa_i)*(hp.sigma_p^-2)*((Y(R_ind)-mu(R_ind))^2) + ...
             -kappa_i + 2);
             % recall log is natural log, log10 is log
 
@@ -127,14 +134,14 @@ while length(I) < totalpts_slimdata         % max pts in Slim Data
     % UPDATE L (dxd) to L_prime (d+1 x d+1), recall L is lower triangular
     L(end+1,:) = (V(:,inclusion_ind))'; % new row
     %this term can sometimes go negative!!!
-    L(end,end+1) = sqrt( K_Function(X(inclusion_ind),X(inclusion_ind)) - p(inclusion_ind) ); % bottom rt scalar
+    L(end,end+1) = sqrt( K_Function(X(inclusion_ind),X(inclusion_ind),hp) - p(inclusion_ind) ); % bottom rt scalar
     
     li = L(end,end); % bottom rt corner of Lprime
     vi = V(:,inclusion_ind); %dx1 col vector
     
 
     % UPDATE V (dxn) to V_prime (d+1 x n, add new row)
-    v = 1/li * (K_Function(X,X(inclusion_ind)) - V'*vi); %nx1 col vector
+    v = 1/li * (K_Function(X,X(inclusion_ind),hp) - V'*vi); %nx1 col vector
     V_prime = [V; v'];
 
 
@@ -143,7 +150,7 @@ while length(I) < totalpts_slimdata         % max pts in Slim Data
     % UPDATE L_M (dxd to d+1 x d+1)
     l_M = inv(L_M)*V*v; %dx1            % INVERSE HERE WILL SLOW US DOWN!
     L_M(end+1,:) = l_M'; % new row 1xd, L_M now (d+1 x d)
-    L_M(end,end+1) = sqrt(sigmasq + v'*v - l_M'*l_M); % bottom rt corner scalar
+    L_M(end,end+1) = sqrt(hp.sigma_p^2 + v'*v - l_M'*l_M); % bottom rt corner scalar
     l_Mi = L_M(end,end);
 
     temp = (inv(L_M)*V_prime);
@@ -169,26 +176,8 @@ end
 % end the while loop, should have a final active set I
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% NOW DO FULL GPR
-
-%%%%%% Execute FULL GPR
-
-% FULL GPR
-% Use either cholesky decomp -->    calcGPR_chol()
-% or regular inverse with inv() --> calcGPR()
-GPR_full = calcGPR(X,Y,nnum,"inv");
-%ylim([-4, 4]);
-
-% FULL GPR ON DOWNSAMPLED DATA
-% Create a Downsampled Dataset N -> M
-b = 4; % keep every jth point
-[X_d,Y_d] = rawDownsample(X,Y,b);
-GPR_d = calcGPR(X_d,Y_d,nnum,"inv");
-%ylim([-4, 4]);
-
-
-
-%%%%%%%%%%  CREATE SLIMDATA PLOTS
+%%%%%%%%%%  CREATE PLOTS
+close all
 
 % Plot 1: Training DataPoints
  % Show pts in I (included)
@@ -206,49 +195,47 @@ title(title_str), grid on
 %f.WindowState = 'maximized'; %make it full screen
 
 
-% PREDICTION Points!
+%%%%% PREDICTIONS!
 f2 = figure;
 plot(X(I),Y(I),'bo','markerfacecolor','b'), hold on
 xlabel('Training Data X Values'),ylabel('Training Data Y Values')
 
 % k prediction points
 X_beg = -nnum; X_end = nnum; 
-X_star = [(-15+X_beg):2:(15+X_end)];
+X_Star = [(-15+X_beg):.2:(15+X_end)];
 
-for k = 1:length(X_star)
+for k = 1:length(X_Star)
 
-    l_star = inv(L)*(K_Function(X_star(k),X(I)))';
+    l_star = inv(L)*(K_Function(X_Star(k),X(I),hp))';
     l_Mstar = inv(L_M)*l_star;
 
     Y_mu_star(k) = l_Mstar'*Beta;
-    Y_sigmasq_star(k) = K_Function(X_star(k), X_star(k)) + ...
+    Y_sigmasq_star(k) = K_Function(X_Star(k), X_Star(k), hp) + ...
         - norm(l_star)^2 + ...
-        sigmasq*norm(l_Mstar)^2;
+        hp.sigma_p^2*norm(l_Mstar)^2;
 end
+% 
+% errorbar(X_star,Y_mu_star,Y_sigmasq_star,'ro')
+% legend('Slim Data','Prediction \mu and \sigma')
+% title_str = strcat("Approx GPR on Slim Data (", ...
+%     num2str(length(X)),"raw to ", ...
+%     num2str(length(I)),"slim)");
+% title(title_str), grid on
 
-% Plot of GPR w/ SlimData
-errorbar(X_star,Y_mu_star,Y_sigmasq_star,'ro')
-legend('Slim Data','Prediction \mu and \sigma^2')
-title_str = strcat("Approx GPR on Slim Data (", ...
-    num2str(length(X)),"raw to ", ...
-    num2str(length(I)),"slim)");
-title(title_str), grid on
+Y_Star_Hat = Y_mu_star;
+Y_Star_Var = Y_sigmasq_star;
+
+sortobj = [X_Star', Y_Star_Hat', Y_Star_Var'];
+sortobj = sortrows(sortobj);
+sorted.X_Star = sortobj(:,1); sorted.Y_Star_Hat = sortobj(:,2); sorted.Y_Star_Var = sortobj(:,3);
 
 
+f3 = figure;
+p1 = plot(sorted.X_Star,sorted.Y_Star_Hat + 2*sqrt(sorted.Y_Star_Var),'r','LineWidth',2); hold on %upper bound
+plot(sorted.X_Star,sorted.Y_Star_Hat - 2*sqrt(sorted.Y_Star_Var),'r','LineWidth',2); % lower bound
+p2 = plot(sorted.X_Star,sorted.Y_Star_Hat,'r--','Linewidth',2); % prediction means
+p3 = plot(X(I),Y(I),'bo','markerfacecolor','b'); % pts in I
+p4 = plot(X(R),Y(R),'ko'); % pts in R
+xlabel('X Values'), ylabel('Y Values'), title('Gaussian Process Regression')
+grid on, legend([p3 p4 p2 p1],"Slim Data","Excess Raw Pts","Prediction \mu","Prediction 2\sigma")
 
-
-%%%%%%%%%%%%%%%%%%%%%%%% METRICS OF FIT!
-% Comparing SlimData to Downsampled Data
-
-% Downsampled vs. Full 
-Metrics.Downsample.SSD = sum((GPR_full.Y_Star_Hat - GPR_d.Y_Star_Hat).^2);
-Metrics.Downsample.SST = sum((GPR_full.Y_Star_Hat - ones(GPR_full.npredict,1)*mean(GPR_full.Y_Star_Hat)).^2);
-Metrics.Downsample.Rsqr = 1 - Metrics.Downsample.SSD / Metrics.Downsample.SST;
-
-% SlimData vs. Full
-Metrics.Slimdata.SSD = sum((GPR_full.Y_Star_Hat - Y_mu_star').^2);
-Metrics.Slimdata.SST = sum((GPR_full.Y_Star_Hat - ones(GPR_full.npredict,1)*mean(GPR_full.Y_Star_Hat)).^2);
-Metrics.Slimdata.Rsqr = 1 - Metrics.Slimdata.SSD / Metrics.Slimdata.SST;
-
-disp(Metrics.Downsample.Rsqr);
-disp(Metrics.Slimdata.Rsqr);
