da5edb3b4016187ade42e9d171156da0abe58d17
Phil Parisi <philparisi@uri.edu>
2023-03-27 22:02:34 -0400

    pcd results

--- pcl_analysis/pcl_exp.m
+++ pcl_analysis/pcl_exp.m
@@ -28,3 +28,41 @@ ptLikelihood = likelihood_num / likelihood_den
 likelihood_num = exp(-0.5 * (7.5 - 8)^2 / (0.1 + 0.1) );
 likelihood_den = sqrt(2*pi * (0.1 + 0.1) );
 ptLikelihood = likelihood_num / likelihood_den
+
+
+%%
+
+% add paths
+if ispc()
+    addpath("..\results\PCD\")
+    addpath("..\pcl_functions\")
+else
+    addpath("../results/PCD/")
+    addpath("../pcl_functions/")
+end
+
+% what are we comparing today? names of folders
+exact_name = "exact_100_inference";
+exact_num_pcds = length(dir(fullfile(strcat("../results/PCD/",exact_name,"/block_size_400/predictions"), '*.pcd')));
+
+approx_name = "average_50_inference";
+approx_num_pcds = length(dir(fullfile(strcat("../results/PCD/",approx_name,"/block_size_400/predictions"), '*.pcd')));
+
+%%
+exact_pcd = [];
+
+% load exact PCDs
+for i = 0:(exact_num_pcds-1)
+    exact_cloud_path = strcat("../results/PCD/",exact_name,"/block_size_400/predictions/cloud_",num2str(i),".pcd");
+    temp_pcd = double(loadpcd(exact_cloud_path));
+    exact_pcd = [exact_pcd; temp_pcd'];
+end
+
+
+approx_pcd = [];
+% load approx PCDs
+for i = 0:(approx_num_pcds-1)
+    approx_cloud_path = strcat("../results/PCD/",approx_name,"/block_size_400/predictions/cloud_",num2str(i),".pcd");
+    temp_pcd = double(loadpcd(approx_cloud_path));
+    approx_pcd = [approx_pcd; temp_pcd'];
+end
---
+++ post_processing/pointcloud_error_analysis.m
@@ -0,0 +1,94 @@
+% Pointcloud Error Analysis
+% for comparing an exact method with all data points to an approximate
+% method with downsampled data points
+
+% ros script outputs a bunch of pcd's, need to assemble those and compare
+% only the pcd's that are held in common
+
+% currently only works on Ubuntu sorry players
+
+clc, clearvars, close all
+
+%%%%%% SETUP
+
+% add paths
+if ispc()
+    addpath("..\results\PCD\")
+    addpath("..\pcl_functions\")
+else
+    addpath("../results/PCD/")
+    addpath("../pcl_functions/")
+end
+
+% what are we comparing today? names of folders
+exact_name = "exact_100_inference";
+exact_num_pcds = length(dir(fullfile(strcat("../results/PCD/",exact_name,"/block_size_400/predictions"), '*.pcd')));
+
+approx_name = "average_10_inference";
+approx_num_pcds = length(dir(fullfile(strcat("../results/PCD/",approx_name,"/block_size_400/predictions"), '*.pcd')));
+
+
+%%%%%% SORT THE PCDs TO ISOLATE ONLY THE TILES IN COMMON 
+
+matches = 0;
+exact_pcd = [];
+approx_pcd = [];
+
+% go thru the approx PCD
+for i = 0:(approx_num_pcds-1)
+
+
+    % load approx PCD
+    approx_cloud_path = strcat("../results/PCD/",approx_name,"/block_size_400/predictions/cloud_",num2str(i),".pcd");
+    approx_temp = double(loadpcd(approx_cloud_path))';
+    approx_temp_xy = approx_temp(:,1:2);
+
+
+    % find the matching exact PCD
+    for j = 0:(exact_num_pcds-1)
+
+    % load exact PCD
+    exact_cloud_path = strcat("../results/PCD/",exact_name,"/block_size_400/predictions/cloud_",num2str(j),".pcd");
+    exact_temp = double(loadpcd(exact_cloud_path))';
+    exact_temp_xy = exact_temp(:,1:2);
+
+
+    % compare temp PCDs 
+    testing = approx_temp_xy == exact_temp_xy;
+
+    if sum(sum(testing)) == 2*length(testing)
+        % we have a match!
+
+        % add point cloud to their commanders
+        exact_pcd = [exact_pcd; exact_temp];
+        approx_pcd = [approx_pcd; approx_temp];
+        matches = matches + 1;
+        break % get out of searching for the exact pcd
+
+    else
+        % no match :{
+        % keep looping through exact PCDs
+        
+    end
+
+    end
+
+end
+
+disp(strcat("done matching clouds! match #: ", num2str(matches)))
+
+
+%%%%%% CALCULATE DIFFERENCES
+
+% Root Mean Square Error
+diff = approx_pcd(:,3) - exact_pcd(:,3);
+sqdiff = diff.^2;
+
+RMSE = sqrt( mean(sqdiff) );
+
+
+% Mean Absolute error
+abs_diff = abs(approx_pcd(:,3) - exact_pcd(:,3));
+MAE = mean(abs_diff,'all');
+
+
