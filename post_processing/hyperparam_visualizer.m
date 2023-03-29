a058d426f018c9ecf2203c326e45fc712af8b46c
Phil Parisi <philparisi@uri.edu>
2023-03-29 14:45:33 -0400

    updated lml

--- post_processing/hyperparam_visualizer.m
+++ post_processing/hyperparam_visualizer.m
@@ -12,7 +12,7 @@ end
 
 %method = ["random","hybrid","kmeans","neighbor"];
 %percents = 10:10:90;
-method = ["neighbor"];
+method = ["random"];
 percents = ["20_big","30_big","10_big",10,20,30,40,50,60,70,80,90];
 
 
@@ -56,7 +56,7 @@ end
 
 
 % load the csv
-filename = "systematic_10_lml";
+filename = "random_10_lml";
 data = readtable(strcat(filename,".csv"));
 %data = readtable("exact_100_lml.csv");
 % remove rows that have NAN LML
@@ -65,7 +65,7 @@ data = data(~to_remove,:);
 data = sortrows(data,'lml','descend');
 
 
-final_ind = round(0.9*length(data.lml));
+final_ind = round(0.8*length(data.lml));
 
 % output the max lml value
 [lml_max, ind_max] = max(data.lml);
@@ -75,7 +75,7 @@ fprintf("max lml is %d at length=%d and process=%d \n",lml_max,data.length_scale
 % make a scatter plot
 %close all
 figure()
-scatter3(data.length_scale(1:final_ind),data.process_noise(1:final_ind),data.lml(1:final_ind),10,'b.'), hold on
+scatter3(data.length_scale(1:final_ind),data.process_noise(1:final_ind),data.lml(1:final_ind),30,'b.'), hold on
 scatter3(data.length_scale(ind_max),data.process_noise(ind_max),lml_max,'ro','filled')
 xlabel('lengthscale'),ylabel('process noise \sigma_f'),zlabel('LML')
 title(filename)
